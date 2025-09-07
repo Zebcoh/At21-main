ec4f5c9afb34f9ac18600c4abe6029b907f5bfa9
Zebcoh <zebcoh@gmail.com>
2025-09-07 15:33:30 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,19 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
-import HomeView from './views/Home.vue'
+import HomeView from './views/HomeView.vue' // <- bestaat nu echt
 
-const routes = [
-  { path: '/', component: HomeView }
-]
+const routes = [{ path: '/', component: HomeView }]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
-const app = createApp(App)
-
-app.use(router)
-app.use(i18n)
-app.mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
