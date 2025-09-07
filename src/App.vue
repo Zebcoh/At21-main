45944d8ac1d6a2e0999d9683d331ab4be30430a3
Zebcoh <zebcoh@gmail.com>
2025-09-07 15:35:05 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,17 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+</script>
+
 <template>
   <Header />
-  <main>
-    <HomeView />
+  <main class="container">
+    <RouterView /> <!-- Router rendert hier HomeView.vue -->
   </main>
-  <br />
-  <br />
-  <br />
-
   <Footer />
 </template>
 
 <style>
 @import '@/assets/main.css';
 @import '@/assets/home.css';
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
-
-<script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import HomeView from './views/HomeView.vue';
-export default {
-  name: 'App',
-  components: {
-    HomeView
-  },
-  setup() {}
-}
-</script>
