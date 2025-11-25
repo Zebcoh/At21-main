ce2cec83de789f3ce081910c03651dde66638802
Zebcoh <zebcoh@gmail.com>
2025-11-25 22:06:39 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,10 +10,25 @@
     <div class="navbar">
       <h2>at21hairstudio <strong class="katakana-header">ツ</strong></h2>
 
-      <div>
+      <nav class="nav-menu">
+        <RouterLink to="/" class="nav-link">Haarstudio</RouterLink>
+        <RouterLink to="/schoonheidsbehandelingen" class="nav-link">
+          Schoonheidsbehandelingen
+        </RouterLink>
+        <a
+          class="nav-link nav-link-cta"
+          href="https://widget.salonhub.nl/a/at21hairstudio/enschede/link.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Afspraak maken
+        </a>
+      </nav>
+
+      <div class="nav-socials">
         <a href="https://www.instagram.com/at21hairstudio">
           <img
-            src="..\assets\images\instagram.svg"
+            src="../assets/images/instagram.svg"
             class="social-media-links"
             alt=""
             width="30px"
@@ -24,21 +39,19 @@
         </a>
         <a href="https://www.tiktok.com/@at21hairstudio">
           <img
-            src="..\assets\images\tiktok.svg"
+            src="../assets/images/tiktok.svg"
             class="social-media-links"
             alt=""
             width="30px"
             height="30px"
           />
         </a>
-      
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {}
 </script>
 
