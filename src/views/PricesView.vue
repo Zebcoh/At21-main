1dfcfaf16efa2b5c84632a1258b4d9337b670b7b
Zebcoh <zebcoh@gmail.com>
2026-04-29 15:40:11 +0200

    Update PricesView.vue

--- src/views/PricesView.vue
+++ src/views/PricesView.vue
@@ -6,8 +6,9 @@
         <h1 class="at21-header-text">Behandelingen & prijzen</h1>
 
         <p class="at21-header-p">
-          Hieronder vind je onze prijzen. Twijfel je wat je nodig hebt (bijv. lengte/dikte of een combi)?
-          Stuur gerust even een berichtje, dan denken we met je mee.
+          Heb je vragen over onze prijzen of behandelingen? 
+          Je bent altijd welkom om te bellen of even langs te komen. 
+          Voor kleuren, highlights en balayage raden we aan om langs te komen, zodat we een goede en persoonlijke prijsinschatting kunnen maken.
         </p>
 
         <a
@@ -34,7 +35,7 @@
       </div>
 
       <p class="prices-note">
-        * Prijzen zijn in euro’s en kunnen variëren bij extra lengte/dikte of extra product.
+        * Afspraken die niet minimaal 24 uur van tevoren worden geannuleerd, kunnen voor 50% worden doorberekend.
       </p>
     </div>
   </section>
@@ -45,84 +46,40 @@ const sections = [
   {
     title: 'Heren',
     items: [
-      { name: 'Wassen & knippen', price: '€ 32,95' },
-      { name: 'Krullen (kort)', price: '€ 43,75' },
-      { name: 'Krullen (lang)', price: '€ 69,95' },
+      { name: 'Wassen & knippen', price: '€ 36.50' },
+      { name: 'Krullen (kort)', price: '€ 47.75' },
+      { name: 'Krullen (lang)', price: '€ 73,95' },
+      { name: 'Bijwerken', price: '€ 22,50' },
     ],
   },
   {
     title: 'Dames',
     items: [
-      { name: 'Knippen', price: '€ 36,95' },
-      { name: 'Wassen & knippen', price: '€ 44,95' },
-      { name: 'Wassen, knippen & föhnen', price: '€ 52,95' },
-      { name: 'Krullen', price: '€ 69,95' },
-      { name: 'Wassen & föhnen', price: '€ 32,75' },
+      { name: 'Knippen', price: '€ 39,95' },
+      { name: 'Wassen & knippen', price: '€ 48,50' },
+      { name: 'Wassen, knippen & föhnen', price: '€ 59,95' },
+      { name: 'Krullen', price: '€ 73,95' },
+      { name: 'Wassen & föhnen', price: '€ 39,50' },
     ],
-  },
-  {
-    title: 'Kinderen',
-    items: [{ name: 'Knippen', price: '€ 25,00' }],
   },
   {
     title: 'Extra’s',
     items: [
-      { name: 'Bijwerken', price: '€ 15,00' },
-      { name: 'Pony', price: '€ 7,50' },
-      { name: 'Wassen & drogen', price: '€ 19,50' },
+      { name: 'Knippen Kinderen', price: '€ 30,00' }],
+      { name: 'Bijwerken', price: '€ 22,50' },
+      { name: 'Pony', price: '€ 15' },
+      { name: 'Wassen & drogen', price: '€ 22,95' },
     ],
   },
   {
     title: 'Kleur',
     items: [
-      { name: 'Shades 10cc', price: '€ 13,75' },
-      { name: 'Shades 30cc', price: '€ 32,75' },
-      { name: 'Uitgroei 10cc', price: '€ 15,25' },
-      { name: 'Uitgroei 30cc', price: '€ 41,25' },
-      { name: 'Uitgroei + knippen + drogen', price: '€ 86,20' },
-      { name: 'Uitgroei + knippen + föhnen', price: '€ 98,20' },
+      { name: 'Kleuren', price: ' vanaf € 74,75' },
+      { name: 'Highlights', price: ' vanaf € 129,75' },
+      { name: 'Balayage', price: 'vanaf € 189,75' },
     ],
   },
-  {
-    title: 'Basis kleuring',
-    items: [
-      // Kort
-      { name: 'Kort: kleuring & drogen', price: '€ 45,00' },
-      { name: 'Kort: kleuring & föhnen', price: '€ 60,00' },
-      { name: 'Kort: kleuring + knippen & drogen', price: '€ 75,00' },
-      { name: 'Kort: kleuring + knippen & föhnen', price: '€ 90,00' },
 
-      // Halflang
-      { name: 'Halflang: kleuring & drogen', price: '€ 50,00' },
-      { name: 'Halflang: kleuring & föhnen', price: '€ 65,00' },
-      { name: 'Halflang: kleuring + knippen & drogen', price: '€ 80,00' },
-      { name: 'Halflang: kleuring + knippen & föhnen', price: '€ 95,00' },
-
-      // Lang
-      { name: 'Lang: kleuring & drogen', price: '€ 60,00' },
-      { name: 'Lang: kleuring & föhnen', price: '€ 80,00' },
-      { name: 'Lang: kleuring + knippen & drogen', price: '€ 90,00' },
-      { name: 'Lang: kleuring + knippen & föhnen', price: '€ 110,00' },
-    ],
-  },
-  {
-    title: 'Highlights',
-    items: [
-      { name: 'Bovenop', price: '€ 44,75' },
-      { name: 'Half', price: '€ 65,00' },
-      { name: 'Totaal', price: '€ 86,00' },
-      { name: 'Lang haar: totaal', price: '€ 119,95' },
-    ],
-  },
-  {
-    title: 'Bruid & opsteken',
-    items: [
-      { name: 'Opsteken (half)', price: '€ 21,95' },
-      { name: 'Opsteken (lang)', price: '€ 41,95' },
-      { name: 'Proefkapsel bruid', price: '€ 79,95' },
-      { name: 'Bruidskapsel', price: '€ 144,75' },
-    ],
-  },
 ]
 </script>
 
